e4fa0a9f33541557119cd0412d88ef7066df87b7
Wesley de Matos Souza <wesleydematos3@gmail.com>
2022-12-14 15:56:22 -0300

    feat: create route get /users

--- src/routers/users/index.ts
+++ src/routers/users/index.ts
@@ -1,6 +1,17 @@
 import { Router } from "express";
-import { createUserController } from "../../controllers/users";
+import {
+  createUserController,
+  listUsersController,
+} from "../../controllers/users";
+import { ensureAuthMiddleWare } from "../../middlewares/users/ensureAuthMiddleware";
+import { ensureRequesterIsAdmMiddleware } from "../../middlewares/users/ensureRequesterIsAdmMiddleware";
 
 export const userRouters = Router();
 
 userRouters.post("", createUserController);
+userRouters.get(
+  "",
+  ensureAuthMiddleWare,
+  ensureRequesterIsAdmMiddleware,
+  listUsersController
+);
