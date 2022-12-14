8ac3a791e8bab32e6021136f31074333382b85c5
Wesley de Matos Souza <wesleydematos3@gmail.com>
2022-12-14 13:04:10 -0300

    feat: add routers

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
-import "reflect-metadata"
-import express from "express"
+import "reflect-metadata";
+import express from "express";
+import { sessionRouter } from "./routers/session";
+import { userRouters } from "./routers/users";
 
+const app = express();
 
+app.use(express.json());
+app.use("/users", userRouters);
+app.use("/login", sessionRouter);
 
-const app = express()
-app.use(express.json())
-
-
-export default app
+export default app;
