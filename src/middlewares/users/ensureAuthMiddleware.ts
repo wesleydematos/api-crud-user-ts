5f8fee4760c44c92104ed70f1a28df71d7847cbd
Wesley de Matos Souza <wesleydematos3@gmail.com>
2022-12-14 15:54:37 -0300

    feat: create Auth Middleware

---
+++ src/middlewares/users/ensureAuthMiddleware.ts
@@ -0,0 +1,31 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import "dotenv/config";
+
+export const ensureAuthMiddleWare = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  let token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({
+      message: "Invalid token",
+    });
+  }
+
+  token = token.split(" ")[1];
+
+  jwt.verify(token, process.env.SECRET_KEY!, (error, decoded: any) => {
+    if (error) {
+      return res.status(401).json({
+        message: error.message,
+      });
+    }
+
+    req.requesterId = decoded.sub;
+
+    return next();
+  });
+};
