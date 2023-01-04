e3141a3e8c39a17ac73df5ed8b5a28f72ea35389
Wesley de Matos Souza <wesleydematos3@gmail.com>
2023-01-04 15:08:02 -0300

    fix: remove map

--- src/services/users/listUsersService.ts
+++ src/services/users/listUsersService.ts
@@ -4,9 +4,17 @@ import { User } from "../../entities/userEntity";
 export const listUsersService = async () => {
   const userRepository = AppDataSource.getRepository(User);
 
-  const users = await userRepository.find();
-
-  users.map((e: any) => delete e.password);
+  const users = await userRepository.find({
+    select: [
+      "id",
+      "name",
+      "email",
+      "isAdm",
+      "createdAt",
+      "updatedAt",
+      "isActive",
+    ],
+  });
 
   return users;
 };
